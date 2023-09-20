50927597b6d79f3ab121798bae20087a0a92131c
anhnhu <anhnt@subwallet.app>
2023-09-20 09:56:41 +0700

    begin code

--- scripts/addonData/companyList.js
+++ scripts/addonData/companyList.js
@@ -12,7 +12,7 @@ const getData = async () => {
 
     // console.log($('table.table_content tbody tr'))
     const data = [];
-     $('table.table_content tbody tr').each((i, element) => {
+    $('table.table_content tbody tr').each((i, element) => {
         const tds = $(element).find('td')[0];
         const divs = $(tds).find('div');
         let name = '';
@@ -20,15 +20,15 @@ const getData = async () => {
         if (divs.length > 0) {
             name = $(divs[1]).text();
             code = $(divs[0]).text();
-            console.log(name, code)
+
+            data.push({
+                name,
+                code,
+                codeFull: code.toUpperCase()
+            })
         }
-        data.push({
-            name,
-            code,
-            codeFull: code.toUpperCase()
-        })
     })
-      await fs.writeFile('data/addonData/companyList.json', JSON.stringify(data, null, 2));
-  console.log(`You are save dataa companyList.json`);
+    await fs.writeFile('data/addonData/companyList.json', JSON.stringify(data, null, 2));
+    console.log(`You are save dataa companyList.json`);
 }
 getData()
